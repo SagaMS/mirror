044dfcb0e080553109dc20d12acdec993598b3ec
SagaMS <91miablond@gmail.com>
2024-09-28 19:50:19 +0700

    feat:add some functional

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,30 +1,56 @@
-let apiUrl ='https://favqs.com/api/qotd';
-function writeACitate(quote, author){
-    const q  = document.getElementsByClassName('quote')[0]
-    const a = document.getElementsByClassName('author')[0]
-    q.innerHTML = quote
-    a.innerHTML= author
+// let form = document.querySelector(".search-form");
+// function showImage(arr){
+//   const img = document.getElementsByClassName('image')
+//   console.log(img)
+//   for (let index = 0; index < img.length; index++) {
+//     const element = img[index];
+//      element.src = arr[index]['url_m']
+//      element.alt = arr[index]['title']
+//   }
+// }
 
+
+// form.addEventListener("submit", function (e) {
+// e.preventDefault() 
+
+// let formdata = new FormData(this);
+// let tag = formdata.get("image-search");
+
+
+// const url =`https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=0f15ff623f1198a1f7f52550f8c36057&tags=${tag}&tag_mode=all&extras=url_m&format=json&nojsoncallback=1`
+
+
+
+// async function getData() {
+//   const res = await fetch(url);
+//   const data = await res.json();
+
+//   showImage(data['photos']['photo'])
+
+// }
+// getData();
+
+// });
+
+
+
+const search = document.getElementById('search')
+const magnify = document.querySelector('.magnify')
+search.focus()
+
+
+function jennaortega(){
+  
+    alert("Jenna Ortega")
 }
 
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    
-    writeACitate(data['quote']['body'],data['quote']['author'] )
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+search.addEventListener('keypress', function(e){
+  if (e.key == "Enter") {
+    jennaortega();
+ }
+}
 
-// console.log(d)
-
-//   console.log(data["quote"][8]);
-//   console.log(data["quote"][10]);
+)
 
 
+magnify.addEventListener('click', jennaortega)
